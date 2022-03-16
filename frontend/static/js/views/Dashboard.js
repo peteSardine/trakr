3d7ee08e54cd5b3d568feaa68c3ead686d594c09
peteSardine <pete@sardine.ai>
2022-03-15 17:53:17 -0700

    commit

---
+++ frontend/static/js/views/Dashboard.js
@@ -0,0 +1,21 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("Dashboard");
+    }
+
+    async getHtml()
+    {   
+        return `
+        <h1>Dashboard</h1>
+
+        <p>
+        Welcome to the dashboard.
+        </p>
+
+        `;
+
+    }
+}
